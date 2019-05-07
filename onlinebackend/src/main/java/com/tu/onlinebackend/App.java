b75636b41ac665ea8aad30991aece4ca9d51d17e
Yousif Osman <yousif.osman.elawad@gmail.com>
2019-05-07 00:33:31 -0500

    My Project setup commited sucessfully

---
+++ onlinebackend/src/main/java/com/tu/onlinebackend/App.java
@@ -0,0 +1,13 @@
+package com.tu.onlinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
